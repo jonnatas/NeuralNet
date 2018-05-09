f6ea86801acb83f156aaf028b9b374af053639ed
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2018-05-09 20:40:33 -0300

    added scaler and spliting data into test and train

--- nn_test.py
+++ nn_test.py
@@ -1,13 +1,28 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 import numpy
+import os
+
 numpy.random.seed(7)
 
 #carrregando dataser
 dataset = numpy.loadtxt("diabetes.csv", delimiter=",")
+
+# Separando os dados em entrada e saida
+
 X = dataset[:, 0:8]
 Y = dataset[:, 8]
-# Separando os dados em entrada e saida
+
+#Aplicando scaler pra normalizar dados
+
+scaler = StandardScaler()
+X_scaler = scaler.fit_transform(X)
+
+#Separando dados pra treino e teste
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_scaler, Y, test_size=0.2, random_state=10)
 
 
 #Definindo o modelo
@@ -26,15 +41,15 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Treinando o modelo 
-model.fit(X, Y, epochs=150, batch_size=10)
+model.fit(X_train, Y_train, epochs=150, batch_size=10)
 
 #Avaliando o modelo
-scores = model.evaluate(X, Y)
+scores = model.evaluate(X_train, Y_train)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 #Predict
 
-predictions = model.predict(X)
+#predictions = model.predict(X)
 
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
+#rounded = [round(x[0]) for x in predictions]
+#print(rounded)
